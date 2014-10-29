b110118ed160e857aa7c03e6258eed3b402d0b48
Ijeoma Oguegbu <ijeoma.oguegbu@meltwater.org>
2014-10-29 12:49:37 +0000

    user model added without password

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141029122204) do
+ActiveRecord::Schema.define(version: 20141029124827) do
 
   create_table "goals", force: true do |t|
     t.string   "title"
     t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "username"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
