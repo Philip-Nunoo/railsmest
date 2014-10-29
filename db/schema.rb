c44711ac3dfbe4504813c0a229366f6555c087c0
Ijeoma Oguegbu <ijeoma.oguegbu@meltwater.org>
2014-10-29 21:35:31 +0000

    changes to the user migration file

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141029210409) do
+ActiveRecord::Schema.define(version: 20141029213105) do
 
   create_table "goals", force: true do |t|
     t.string   "title"
