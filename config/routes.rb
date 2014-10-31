7c00dd3715c06ee0f750bc3407812284cb2e4f24
Philip A. Nunoo <ketay99@gmail.com>
2014-10-31 17:01:50 +0000

    Worked on home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'welcome/index'
-  get 'welcome/sign_in'
+  get 'login' => 'welcome#sign_in', :as=> 'login'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
