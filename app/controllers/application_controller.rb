51c92ff6c21c9e911c9ef783847db6842faec542
kelechiudoagwu <kelechi.udoagwu@meltwater.org>
2014-11-02 21:47:43 +0000

    finished work on sign in processes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  protected
+  def authenticate_pass!
+  	if pass_signed_in?
+  		super
+  	else
+  		flash[:notice] = "Please sign in to continue"	
+  		redirect_to :login and return
+  	end
+  end
 end
